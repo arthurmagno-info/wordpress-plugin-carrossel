cce9f4101ac5ab14927c7f770c4068fd2cf91aac
Arthur Magno <arthurmagno@arthurmagno.com>
2021-12-13 20:43:23 -0300

    Criação da estrutura

---
+++ carrossel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Carrossel
+ * Plugin URI:      www.arthurmagno.com
+ * Description:     Corrossel de posts
+ * Author:          Arthur Magno
+ * Author URI:      www.arthurmagno.com
+ * Text Domain:     carrossel
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Carrossel
+ */
